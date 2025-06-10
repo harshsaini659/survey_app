4c1598edb8badfb235caecf8947495731a5bc240
Harsh saini <harshsaini555333@gmail.com>
2025-06-11 00:55:24 +0530

    create survey model and migration file

---
+++ migrations/20250610184222-create-survey.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Surveys', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      targetGender: {
+        type: Sequelize.STRING
+      },
+      minAge: {
+        type: Sequelize.INTEGER
+      },
+      maxAge: {
+        type: Sequelize.INTEGER
+      },
+      coordinatorId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Surveys');
+  }
+};
